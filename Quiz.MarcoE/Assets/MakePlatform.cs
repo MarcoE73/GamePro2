f741d24a6ff98cb1261b5d1b4712c38380c454d2
MarcoE73 <MarcoEspinoza262318@gmail.com>
2019-03-28 22:51:05 -0400

    Quiz

---
+++ Quiz.MarcoE/Assets/MakePlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePlatform : MonoBehaviour {      public GameObject platform;     float timer = 0;
+ 
+    void Start()
+    {
+
+    }
+     void Update()     {
+         timer += Time.deltaTime;          if (timer > 1)         {             GameObject p;
+
+
+             p = Instantiate(platform, new Vector2(Random.Range(-12, 10), Random.Range(-8,7)), Quaternion.identity);
+
+            
+
+
+            timer = 0;
+             p.transform.localScale = new Vector3(Random.Range(3,7), .2f);         }     } }  
---
+++ Quiz.MarcoE/Assets/RandomColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomColor : MonoBehaviour
+{
+    public static Color newcolor;
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        newcolor = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        gameObject.GetComponent<Renderer>().material.color = newcolor;
+
+    }
+
+}
